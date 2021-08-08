322ef04b4e5412206addc2bb28dfb2c38a71e005
kunallad54 <kunallad5473@gmail.com>
2021-08-08 18:55:29 +0530

    Ability to create HelloWorld Spring App and display output in web

--- SpringDemoWithSTS/HelloWorldDemo/src/main/java/com/example/demo/HelloWorldDemoApplication.java
+++ SpringDemoWithSTS/HelloWorldDemo/src/main/java/com/example/demo/HelloWorldDemoApplication.java
@@ -1,7 +1,9 @@
 /**
- * Purpose : Create a Helloworld Spring App to 
- * 			 display “Hello from BridgeLabz”
- * 			 using STS
+ * UC 2 : Create a Helloworld Spring App to display “Hello from 
+ * 		  BridgeLabz” in a Web Page
+ * 		 - Use MVC Controller to invoke a View i.e. HTML 
+ * 		   when request is received and also pass Model to be displayed. 
+ * 		 - Use Thymeleaf to display the Model
  * 
  * @author Krunal Lad
  * @since 08-08-2021
---
+++ SpringDemoWithSTS/HelloWorldDemo/src/main/java/com/example/demo/controller/HelloWebController.java
@@ -0,0 +1,44 @@
+/**
+ * UC 2 : Create a Helloworld Spring App to display “Hello from 
+ * 		  BridgeLabz” in a Web Page
+ * 		 - Use MVC Controller to invoke a View i.e. HTML 
+ * 		   when request is received and also pass Model to be displayed. 
+ * 		 - Use Thymeleaf to display the Model
+ * 
+ * @author Krunal Lad
+ * @since 08-08-2021
+ */
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWebController {
+		
+	/**
+	 * Purpose :@GetMapping annotation maps HTTP GET requests onto specific handler methods. 
+	 * 			It is a composed annotation that acts as a shortcut for
+	 * 		    @RequestMapping(method = RequestMethod.GET).
+	 * 
+	 * @return hello.html page
+	 */
+	@GetMapping("/web")
+	public String hello() {
+		return "hello";
+	}
+	
+	/**
+	 * Purpose : Here we have used ThymeLeaf to display the model
+	 * 
+	 * @param model
+	 * @return message.html page
+	 */
+	@GetMapping("/web/message")
+	public String message(Model model) {
+		model.addAttribute("message", "This is a custom message");
+		return "message";
+	}
+	
+}
