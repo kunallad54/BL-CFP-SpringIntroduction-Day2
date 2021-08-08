faf1169e95d366b2e4519155b40481bc4e054381
kunallad54 <kunallad5473@gmail.com>
2021-08-08 17:58:33 +0530

    Ability to create HelloWorld Spring App in STS and display output

---
+++ SpringDemoWithSTS/HelloWorldDemo/src/main/java/com/example/demo/HelloWorldDemoApplication.java
@@ -0,0 +1,22 @@
+/**
+ * Purpose : Create a Helloworld Spring App to 
+ * 			 display “Hello from BridgeLabz”
+ * 			 using STS
+ * 
+ * @author Krunal Lad
+ * @since 08-08-2021
+ */
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldDemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello from BridgeLabz");
+		SpringApplication.run(HelloWorldDemoApplication.class, args);
+	}
+
+}
